4b56fb2b6d60bdc17ec971f416b4e63b5c1a5113
Richard <st00ka@yahoo.com>
2020-07-17 15:57:30 +0100

    adding build folder to test if can be used as a static site

---
+++ build/precache-manifest.bee0aa2e58fd362d262c704af20b6009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f50dbfd12205c20d3f0d76fde7372a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c38581574c1ae1e936d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "038026d99368c3558e11",
+    "url": "/static/js/2.f76e3c19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f76e3c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38581574c1ae1e936d6",
+    "url": "/static/js/main.a4192264.chunk.js"
+  },
+  {
+    "revision": "d286480b7d0ae503453b",
+    "url": "/static/js/runtime-main.ac7c107e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
